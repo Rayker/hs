8c2d05f6fc9f17bd60d242ebae519016d4984ffa
Tatarinov Denis <denis160995@yandex.ru>
2016-06-21 12:59:55 +0400

    Base

---
+++ hsapi/src/main/java/ru/ardecs/hs/hsapi/HsapiApplication.java
@@ -0,0 +1,12 @@
+package ru.ardecs.hs.hsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsapiApplication.class, args);
+	}
+}
