31b87bf56edda4fef4d304736af951fae36a3d50
Tatarinov Denis <denis160995@yandex.ru>
2016-06-22 11:08:27 +0400

    Annotation fix

--- hsapi/src/main/java/ru/ardecs/hs/hsapi/HsapiApplication.java
+++ hsapi/src/main/java/ru/ardecs/hs/hsapi/HsapiApplication.java
@@ -2,8 +2,12 @@ package ru.ardecs.hs.hsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"ru.ardecs.hs.hsdb.repositories"})
+@EntityScan("ru.ardecs.hs.hsdb.*")
 public class HsapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HsapiApplication.class, args);
--- hsapi/src/main/java/ru/ardecs/hs/hsapi/hello/HelloController.java
+++ hsapi/src/main/java/ru/ardecs/hs/hsapi/hello/HelloController.java
@@ -12,8 +12,6 @@ import ru.ardecs.hs.hsdb.repositories.SpecialityRepository;
 import java.io.IOException;
 
 @RestController
-@EnableJpaRepositories(basePackages = {"ru.ardecs.hs.hsdb.repositories"})
-@EntityScan("ru.ardecs.hs.hsdb.*")
 public class HelloController {
 	@Autowired
 	private SpecialityRepository repository;
@@ -22,9 +20,5 @@ public class HelloController {
 	@ResponseBody
 	public Iterable<Speciality> specialities(Pageable pageable) throws IOException {
 		return repository.findAll(pageable);
-	}
-
-	public void setRepository(SpecialityRepository repository) {
-		this.repository = repository;
 	}
 }
