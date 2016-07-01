62aa1ae83a91e913a9dc933c0ff12bc661541eaa
Tatarinov Denis <denis160995@yandex.ru>
2016-07-01 13:26:27 +0400

    Fixed ApiWrapperImpl

--- hsclient/src/main/java/ru/ardecs/hs/hsclient/api/ApiWrapperImpl.java
+++ hsclient/src/main/java/ru/ardecs/hs/hsclient/api/ApiWrapperImpl.java
@@ -7,7 +7,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -24,6 +23,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -36,7 +36,7 @@ import java.util.List;
 @Component
 public class ApiWrapperImpl implements ApiWrapper {
 	@Autowired
-	private CloseableHttpClient httpclient;
+	private CloseableHttpClient httpClient;
 
 	private URI createUri(String path, List<NameValuePair> nvps) throws URISyntaxException {
 		return new URIBuilder()
@@ -48,17 +48,10 @@ public class ApiWrapperImpl implements ApiWrapper {
 				.build();
 	}
 
-	private CloseableHttpResponse execute(HttpRequestBase httpRequest) throws IOException {
-		return httpclient.execute(httpRequest);
-	}
-
-	private <T> T sendGet(URI uri, Type type) throws IOException {
-		HttpGet httpGet = new HttpGet(uri);
-		CloseableHttpResponse response = httpclient.execute(httpGet);
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-//		Type listType = new TypeToken<T>() {
-//		}.getType();
-		return new Gson().fromJson(rd, type);
+	private <T> T sendGetAndParseResponse(String path, List<NameValuePair> nvps, Type type) throws IOException, URISyntaxException {
+		HttpGet httpGet = createGetRequest(path, nvps);
+		CloseableHttpResponse response = httpClient.execute(httpGet);
+		return parse(response, type);
 	}
 
 	private <T> T parse(CloseableHttpResponse response, Type type) throws IOException {
@@ -67,14 +60,23 @@ public class ApiWrapperImpl implements ApiWrapper {
 	}
 
 	private CloseableHttpResponse sendPost(String path, List<NameValuePair> nvps) throws URISyntaxException, IOException {
+		HttpPost httpPost = createPostRequest(path, nvps);
+		return httpClient.execute(httpPost);
+	}
+
+	private HttpPost createPostRequest(String path, List<NameValuePair> nvps) throws URISyntaxException, UnsupportedEncodingException {
 		HttpPost httpPost = new HttpPost(createUri(path, new ArrayList<>()));
 		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-		return execute(httpPost);
+		return httpPost;
+	}
+
+	private HttpGet createGetRequest(String path, List<NameValuePair> nvps) throws URISyntaxException, UnsupportedEncodingException {
+		return new HttpGet(createUri(path, nvps));
 	}
 
 	@Override
 	public List<Speciality> specialities() throws IOException, URISyntaxException {
-		return this.<ArrayList<Speciality>>sendGet(createUri("/specialities.json", new ArrayList<>()), new TypeToken<List<Speciality>>() {
+		return this.<ArrayList<Speciality>>sendGetAndParseResponse("/specialities.json", new ArrayList<>(), new TypeToken<List<Speciality>>() {
 		}.getType());
 	}
 
@@ -82,10 +84,9 @@ public class ApiWrapperImpl implements ApiWrapper {
 	public List<Hospital> hospitals(HospitalsRequestModel requestModel) throws URISyntaxException, IOException {
 		List<NameValuePair> params = new ArrayList<>();
 		params.add(new BasicNameValuePair("specialityId", String.valueOf(requestModel.getSpecialityId())));
-		URI uri = createUri("/hospitals.json", params);
 		Type type = new TypeToken<List<Hospital>>() {
 		}.getType();
-		return this.<ArrayList<Hospital>>sendGet(uri, type);
+		return this.<ArrayList<Hospital>>sendGetAndParseResponse("/hospitals.json", params, type);
 	}
 
 	@Override
@@ -93,17 +94,17 @@ public class ApiWrapperImpl implements ApiWrapper {
 		List<NameValuePair> params = new ArrayList<>();
 		params.add(new BasicNameValuePair("specialityId", String.valueOf(doctorsRequestModel.getSpecialityId())));
 		params.add(new BasicNameValuePair("hospitalId", String.valueOf(doctorsRequestModel.getHospitalId())));
-		URI uri = createUri("/doctors.json", params);
-		return this.<ArrayList<Doctor>>sendGet(uri, new TypeToken<List<Doctor>>() {
+		return this.<ArrayList<Doctor>>sendGetAndParseResponse("/doctors.json", params, new TypeToken<List<Doctor>>() {
 		}.getType());
 	}
 
 	@Override
 	public List<Date> choseDate(Long doctorId) throws IOException, URISyntaxException {
-
-		return this.sendGet(
-				createUri("/doctors/" + doctorId + "/workdays.json", new ArrayList<>()),
-				new TypeToken<List<Date>>() {}.getType());
+		return this.sendGetAndParseResponse(
+				"/doctors/" + doctorId + "/workdays.json",
+				new ArrayList<>(),
+				new TypeToken<List<Date>>() {
+				}.getType());
 	}
 
 	@Override
@@ -114,8 +115,9 @@ public class ApiWrapperImpl implements ApiWrapper {
 		params.add(new BasicNameValuePair("date", format.format(intervalsRequestModel.getDate())));
 		params.add(new BasicNameValuePair("sessionId", sessionId));
 
-		Type type = new TypeToken<List<VisitModel>>() {}.getType();
-		return this.sendGet(createUri("/visits/all.json", params), type);
+		Type type = new TypeToken<List<VisitModel>>() {
+		}.getType();
+		return this.sendGetAndParseResponse("/visits/all.json", params, type);
 	}
 
 	@Override
@@ -144,7 +146,7 @@ public class ApiWrapperImpl implements ApiWrapper {
 
 	@Override
 	public TicketModel getTicketModel(Long reservedTimeId) throws IOException, URISyntaxException {
-		return this.sendGet(createUri("/visits/" + reservedTimeId + "/ticket.json", new ArrayList<>()), TicketModel.class);
+		return this.sendGetAndParseResponse("/visits/" + reservedTimeId + "/ticket.json", new ArrayList<>(), TicketModel.class);
 	}
 
 	@Override
