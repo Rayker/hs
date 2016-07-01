2049da45e137e44a45f1d770c76d5f08ea2dc350
Tatarinov Denis <denis160995@yandex.ru>
2016-07-01 17:22:08 +0400

    Two api instances

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 call mvn clean
 call mvn package -pl hsapi -am 
-call java -jar hsapi/target/hsapi-1.0-SNAPSHOT.jar 
+start java -jar hsapi/target/hsapi-1.0-SNAPSHOT.jar --server.port=8090 --spring.datasource.url=jdbc:mysql://localhost:3306/hospital_db
+start java -jar hsapi/target/hsapi-1.0-SNAPSHOT.jar --server.port=8091 --spring.datasource.url=jdbc:mysql://localhost:3306/hospital_db_2
 pause
