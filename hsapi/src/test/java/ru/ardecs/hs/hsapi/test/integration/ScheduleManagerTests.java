cc552abd821edc76415eec7470c4c44063f01767
Tatarinov Denis <denis160995@yandex.ru>
2016-07-12 12:11:10 +0400

    Fixed ScheduleManagerTests

--- hsapi/src/test/java/ru/ardecs/hs/hsapi/test/integration/ScheduleManagerTests.java
+++ hsapi/src/test/java/ru/ardecs/hs/hsapi/test/integration/ScheduleManagerTests.java
@@ -1,11 +1,10 @@
 package ru.ardecs.hs.hsapi.test.integration;
 
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,20 +14,45 @@ import ru.ardecs.hs.hsapi.cache.CachedVisit;
 import ru.ardecs.hs.hsapi.test.TestData;
 import ru.ardecs.hs.hscommon.entities.ReservedTime;
 import ru.ardecs.hs.hscommon.models.VisitModel;
+import ru.ardecs.hs.hsdb.repositories.DoctorRepository;
+import ru.ardecs.hs.hsdb.repositories.ReservedTimeRepository;
 
+import java.sql.Date;
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestBeans.class, Mocks.class})
+@ContextConfiguration(classes = {TestBeans.class, ScheduleManagerTests.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Configuration
 public class ScheduleManagerTests {
 	private final TestData testData = new TestData();
-
 	@Autowired
 	private ScheduleManager scheduleManager;
-
 	@Autowired
 	private CacheManager cacheManager;
+
+	@Bean
+	public DoctorRepository doctorRepository() {
+		DoctorRepository mock = mock(DoctorRepository.class);
+		when(mock.findOne(testData.doctorId)).thenReturn(testData.doctor);
+		return mock;
+	}
+
+	@Bean
+	public ReservedTimeRepository reservedTimeRepository() {
+		ReservedTimeRepository mock = mock(ReservedTimeRepository.class);
+		when(mock.findByJobIntervalDoctorIdAndDate(eq(testData.doctorId), any(Date.class)))
+				.thenReturn(Collections.singletonList(testData.reservedTime));
+		return mock;
+	}
 
 	@Test
 	public void cacheTest() {
