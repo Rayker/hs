b66d3159cc24050f87d9cd6968215bc5472ae5c9
Tatarinov Denis <denis160995@yandex.ru>
2016-07-16 09:59:01 +0400

    Fixed tests

--- hsapi/src/test/java/ru/ardecs/hs/hsapi/test/TestBeans.java
+++ hsapi/src/test/java/ru/ardecs/hs/hsapi/test/TestBeans.java
@@ -7,11 +7,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import ru.ardecs.hs.hsapi.Beans;
 import ru.ardecs.hs.hsapi.bl.ScheduleFactory;
 import ru.ardecs.hs.hsapi.cache.MemoryCacheManager;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
 
 import static org.mockito.Mockito.spy;
 
@@ -41,4 +47,21 @@ public class TestBeans {
 	public DatatypeFactory datatypeFactory() throws DatatypeConfigurationException {
 		return DatatypeFactory.newInstance();
 	}
+
+	@Bean
+	public KeyStore keyStore() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keyStore.load(Beans.class.getResourceAsStream("keystore.jks"), "testpass".toCharArray());
+		return keyStore;
+	}
+
+	@Bean
+	public Signature signature(KeyStore keyStore) throws NoSuchAlgorithmException, IOException, InvalidKeySpecException, InvalidKeyException, KeyStoreException, CertificateException, UnrecoverableKeyException {
+		Key key = keyStore.getKey("soap", "testpass".toCharArray());
+		PrivateKey privateKey = (PrivateKey) key;
+		Signature signature = Signature.getInstance("DSAwithSHA1");
+		signature.initSign(privateKey);
+		return signature;
+	}
+
 }
